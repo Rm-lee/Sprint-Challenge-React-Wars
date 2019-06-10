f1865f2f66704e11183bcfcc27b61146e9cbcb9c
Roger Lee <rmlee0003@gmail.com>
2019-06-10 19:56:05 -0400

    added List component to App feeding characters as props

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ListChar from './components/ListChar'
 import './App.css';
 
 class App extends Component {
@@ -22,17 +23,20 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
+        console.log(data);
         this.setState({ starwarsChars: data.results });
       })
       .catch(err => {
         throw new Error(err);
       });
   };
-
+  
   render() {
+  
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <ListChar characters={this.state.starwarsChars}/>
       </div>
     );
   }
