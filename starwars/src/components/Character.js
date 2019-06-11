2789ff87b04eb5bddb861ac47aa363140c9590fd
Roger Lee <rmlee0003@gmail.com>
2019-06-10 20:55:57 -0400

    added functions in character.js to display all keys and values of the character object

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import './ListChar.css'
 
 const Character = props => {
+ 
+ const keyNames = Object.keys(props.character).map(key => {
+  if (key !== "name" ){
+  return (
+      <p className="prop-name">
+          {key}
+      </p>
+      );
+ }
+ });
+ const keyDetails = Object.keys(props.character).map(key => {
+  if (key !== "name" ){
+  return (
+      <p className="prop-details">
+          {props.character[key]}
+      </p>
+      );
+ }});
 
     return (
      <div className="character">
-      {props.character.name}
-      {props.character.homeworld}
+        <h2 className="name">{props.character.name}</h2>
+        <div className="detail-container">
+            <div className="key-names">{keyNames}</div> 
+            <div className="key-details">{keyDetails}</div> 
+        </div>
      </div>
+
     );
 }
 export default Character;
